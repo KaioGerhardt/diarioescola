0822702b157761d35c4b2b8b1a71272118359ea7
Kaio Gerhardt <64344999+KaioGerhardt@users.noreply.github.com>
2023-01-05 20:15:30 -0300

    lista de resolucao - js

---
+++ exercicios.js
@@ -0,0 +1,22 @@
+// código primeiro negocio
+clientes.forEach(element => {
+    let nome = element.nome
+    let format = nome.split(" ")
+    console.log(`${format[format.length - 1]}, ${format[0]}`);
+    
+});
+
+//código da atividade dois de js
+
+var numero = "5(1)9-876-543-21";
+
+var formata = numero.replace(/-/g, "").replace("(", "").replace(")", "").split("")
+
+let ddd = formata[0] + "" + formata[1]
+let digito = formata
+let numeroFormatado = `${formata[3]}${formata[4]}${formata[5]}${formata[6]}-${formata[7]}${formata[8]}${formata[9]}${formata[10]}`
+
+let novoNumero = `(${ddd})_${formata[2]}_${numeroFormatado}`
+
+// a ordem da execucao do log é c e d
+// a function d não tem o metodo resolve()
